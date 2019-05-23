891145bc9fecc7c078f885920d0406b5e968a10b
sa--sondarkar <saisondarkar18@gmail.com>
2019-05-23 22:24:35 +0530

    Category added

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,91 @@
 import 'package:flutter/material.dart';
 
-
-class HexToColor extends Color{
+class HexToColor extends Color {
   static _hexToColor(String code) {
     return int.parse(code.substring(1, 7), radix: 16) + 0xFF000000;
   }
+
   HexToColor(final String code) : super(_hexToColor(code));
 }
 
-class  HelloBox extends StatelessWidget{
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
+class HelloBox extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Center(
       child: Container(
-        color: HexToColor('#D81B60'),
-        width: 300.0,
-        height: 300.0,
-        child: Center(
-         child: Text("Hey Flutty", style: TextStyle(fontSize: 50.0, color: HexToColor('#FAFAFA'),),),
-        )
+          color: HexToColor('#D81B60'),
+          width: 300.0,
+          height: 300.0,
+          child: Center(
+            child: Text(
+              "Hey Flutty",
+              style: TextStyle(
+                fontSize: 50.0,
+                color: HexToColor('#FAFAFA'),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class CustomCategory extends StatelessWidget {
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
+
+  const CustomCategory({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        padding: EdgeInsets.all(10),
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          onTap: () {
+            print('I Tapped');
+          },
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
-
   }
 }
 
@@ -31,12 +94,11 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: HexToColor('#880E4F'),
-          title: Text("Hello Idiot"),
-        ),
-        body: HelloBox()
-      ),
+          appBar: AppBar(
+            backgroundColor: HexToColor('#880E4F'),
+            title: Text("Hello Idiot"),
+          ),
+          body: HelloBox()),
     ),
   );
 }
